2cca2aa611b9300615c87d11c9c56198854bdfd0
robertu94 <robertu@clemson.edu>
2014-09-18 10:43:19 -0400

    Improvements to the set documentation
    +   Meet the requirements in the README for code and documentation

--- hackpack/set/set.cpp
+++ hackpack/set/set.cpp
@@ -1,15 +1,18 @@
 #include <set>
+//Create a set of type_one
 std::set<type_one> myset;
 
 //iterator
 std::set<type_one>::iterator it;
 
-//insert O(N log(N)) or O(N) am for sorted inputs
-myset.insert(type_one (value));
+int main(){
+    //insert O(N log(N)) or O(N) am for sorted inputs
+    myset.insert(type_one (value));
 
-//remove O(log N) or O(1) am post-find
-myset.erase(it);
-myset.erase(key);
+    //remove O(log N) or O(1) am post-find
+    myset.erase(it);
+    myset.erase(key);
 
-//find O(log N)
-myset.find(key)
+    //find O(log N)
+    myset.find(key)
+}
