8e925e479138babd7d37e9c20f9ab96b746b1c6f
robertu94 <robertu@clemson.edu>
2014-10-13 14:51:11 -0400

    Fixed indentation in cowpens

--- hackpack/set/cowpens.cpp
+++ hackpack/set/cowpens.cpp
@@ -16,48 +16,48 @@ vector<pii> list;
 set<pii> myset;
 
 int main (){
-    long N,x,y;
-    cin >> N;
-    for(long i = 0; i < N; i++){
-        cin >> x >> y;
-        list.push_back(make_pair(x,y));
-    }
+	long N,x,y;
+	cin >> N;
+	for(long i = 0; i < N; i++){
+		cin >> x >> y;
+		list.push_back(make_pair(x,y));
+	}
 
-    //Sort the elements on increasing y values
-    sort(list.begin(),list.end(),SortOnY);
+	//Sort the elements on increasing y values
+	sort(list.begin(),list.end(),SortOnY);
 
 	//initialize variables
-    set<pii>::iterator before, after;
-    long area = 0;
+	set<pii>::iterator before, after;
+	long area = 0;
 
 	//Some times it is better to make a change than code an edge case
-    myset.insert(make_pair(0,0));
-    myset.insert(make_pair(1000000,0));
+	myset.insert(make_pair(0,0));
+	myset.insert(make_pair(1000000,0));
 
 	//Use a sweep line based on the set
-    for(vector<pii>::iterator it = list.begin(); it != list.end(); it++){
-        myset.insert(*it);
-        before = myset.lower_bound(*it);
-        after = myset.upper_bound(*it);
-		
+	for(vector<pii>::iterator it = list.begin(); it != list.end(); it++){
+		myset.insert(*it);
+		before = myset.lower_bound(*it);
+		after = myset.upper_bound(*it);
+
 		//lower_bound returns the pointer to the current element
 		//decrement if it will be within bounds
 		if(before != myset.begin()) before--;
 
 		//compute the area of the box
-        area = max(area,  it->second * (after->first - before->first));
-    }
+		area = max(area,  it->second * (after->first - before->first));
+	}
 
 
 	//Check all of the plots that extend to the back of the plot
-    after = myset.begin();
-    for(before = myset.begin(); after != myset.end(); before++){
-        after++;
+	after = myset.begin();
+	for(before = myset.begin(); after != myset.end(); before++){
+		after++;
 
 		//compute the area of the long plots
-        area = max(area, (after->first - before->first) * MAXHEIGHT);
-    }
+		area = max(area, (after->first - before->first) * MAXHEIGHT);
+	}
 
 	//output according to the output spec
-    cout << area << endl;
+	cout << area << endl;
 }
