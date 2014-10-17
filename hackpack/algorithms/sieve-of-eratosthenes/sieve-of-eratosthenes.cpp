1200227afcb4b943a566705eb936085bd7db9e8e
Marshall Clyburn <mardev2013@gmail.com>
2014-10-17 11:50:43 -0400

    Use unsigned ints.
    This prevents an issue with wraparound with huge numbers.

--- hackpack/algorithms/sieve-of-eratosthenes/sieve-of-eratosthenes.cpp
+++ hackpack/algorithms/sieve-of-eratosthenes/sieve-of-eratosthenes.cpp
@@ -5,25 +5,25 @@ using namespace std;
 int main()
 {
 	// Create an array with elements 1..N
-	const int N = 212;
-	int* sieve = new int[N];
-	for(int i = 0; i <= N; i++) sieve[i] = i + 1;
+	const unsigned int N = 10000000;
+	unsigned int* sieve = new unsigned int[N];
+	for(unsigned int i = 0; i <= N; i++) sieve[i] = i + 1;
 
 	// Replace composite numbers with -1
 	// Begin at index 1. Starting the sift at index 0 (1) will
 	// cause all numbers to be marked composite.
-	for(int i = 1; i < N; i++)
+	for(unsigned int i = 1; i < N; i++)
 	{
 		// If this number has been marked, do not use it.
 		if(sieve[i] == -1) continue;
 
 		// Mark composites of the prime.
-		for(int j = pow(sieve[i], 2); j <= N; j += sieve[i])
+		for(unsigned int j = pow(sieve[i], 2); j <= N; j += sieve[i])
 			sieve[j - 1] = -1;
 	}
 
 	// Remaining numbers != -1 are prime.	
-	for(int i = 0; i < N; i++)
+	for(unsigned int i = 0; i < N; i++)
 		if(sieve[i] != -1) cout << sieve[i] << endl;
 
 	return 0;
