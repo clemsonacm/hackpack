833a2e5066eef0db501ae390373b44971df908b8
Marshall Clyburn <mardev2013@gmail.com>
2014-10-17 21:33:43 -0400

    Calculate prefix sum of primes.
    Also add a sample input file.

--- hackpack/algorithms/sieve-of-eratosthenes/problems/sieve-of-eratosthenes.cpp
+++ hackpack/algorithms/sieve-of-eratosthenes/problems/sieve-of-eratosthenes.cpp
@@ -4,26 +4,35 @@ using namespace std;
 
 const size_t N = 10000000;
 bitset<N> sieve;
+int prefix[N];
 
 int main()
 {
-	// Replace composite numbers with -1
+	size_t a, b;
+	cin >> a >> b;
+		
 	// Begin at index 1. Starting the sift at index 0 (1) will
 	// cause all numbers to be marked composite.
+	sieve[0] = 1; // 1 is not prime.
 	for(size_t i = 1; i < N; i++)
 	{
 		// If this number has been marked, do not use it.
 		if(sieve[i]) continue;
-
 		// Mark composites of the prime.
 		for(size_t j = (i + 1) * (i + 1); j <= N; j += (i + 1))
 			sieve[j - 1] = 1;
 	}
 
-	// Remaining numbers != 0 are prime.	
-	sieve.flip();
+	// Compute prefix sum of the number of primes.
+	size_t count = 0;
 	for(size_t i = 0; i < N; i++)
-		if(sieve[i]) cout << i + 1 << endl;
+	{
+		if (!sieve[i]) count++;
+		prefix[i] = count;
+	}
+
+	// Remaining numbers != 0 are prime.	
+	cout << prefix[b - 1] - ( a > 2 ? prefix[a - 2] : 0 ) << endl;
 
 	return 0;
 }
