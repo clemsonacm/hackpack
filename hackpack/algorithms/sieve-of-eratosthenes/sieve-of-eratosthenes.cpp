1533bfd9893f97530403a280f2b3cc0881cb7a88
Marshall Clyburn <mardev2013@gmail.com>
2014-10-17 20:35:47 -0400

    Change from bool to bitset.

--- hackpack/algorithms/sieve-of-eratosthenes/sieve-of-eratosthenes.cpp
+++ hackpack/algorithms/sieve-of-eratosthenes/sieve-of-eratosthenes.cpp
@@ -1,28 +1,29 @@
-#include <cmath>
+#include <bitset>
 #include <iostream>
 using namespace std;
 
-const unsigned int N = 10000000;
-bool* sieve = new bool[N];
+const size_t N = 10000000;
+bitset<N> sieve;
 
 int main()
 {
 	// Replace composite numbers with -1
 	// Begin at index 1. Starting the sift at index 0 (1) will
 	// cause all numbers to be marked composite.
-	for(unsigned int i = 1; i < N; i++)
+	for(size_t i = 1; i < N; i++)
 	{
 		// If this number has been marked, do not use it.
 		if(sieve[i]) continue;
 
 		// Mark composites of the prime.
-		for(unsigned int j = (i + 1) * (i + 1); j <= N; j += (i + 1))
-			sieve[j - 1] = true;
+		for(size_t j = (i + 1) * (i + 1); j <= N; j += (i + 1))
+			sieve[j - 1] = 1;
 	}
 
-	// Remaining numbers != -1 are prime.	
-	for(unsigned int i = 0; i < N; i++)
-		if(!sieve[i]) cout << i + 1 << endl;
+	// Remaining numbers != 0 are prime.	
+	sieve.flip();
+	for(size_t i = 0; i < N; i++)
+		if(sieve[i]) cout << i + 1 << endl;
 
 	return 0;
 }
