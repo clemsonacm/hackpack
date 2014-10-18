0e9a6dd37d8144213d96feb7cf8c0161e677d7a8
Marshall Clyburn <mardev2013@gmail.com>
2014-10-17 20:22:36 -0400

    Use bools.

--- hackpack/algorithms/sieve-of-eratosthenes/sieve-of-eratosthenes.cpp
+++ hackpack/algorithms/sieve-of-eratosthenes/sieve-of-eratosthenes.cpp
@@ -2,29 +2,27 @@
 #include <iostream>
 using namespace std;
 
+const unsigned int N = 10000000;
+bool* sieve = new bool[N];
+
 int main()
 {
-	// Create an array with elements 1..N
-	const unsigned int N = 10000000;
-	unsigned int* sieve = new unsigned int[N];
-	for(unsigned int i = 0; i <= N; i++) sieve[i] = i + 1;
-
 	// Replace composite numbers with -1
 	// Begin at index 1. Starting the sift at index 0 (1) will
 	// cause all numbers to be marked composite.
 	for(unsigned int i = 1; i < N; i++)
 	{
 		// If this number has been marked, do not use it.
-		if(sieve[i] == -1) continue;
+		if(sieve[i]) continue;
 
 		// Mark composites of the prime.
-		for(unsigned int j = pow(sieve[i], 2); j <= N; j += sieve[i])
-			sieve[j - 1] = -1;
+		for(unsigned int j = (i + 1) * (i + 1); j <= N; j += (i + 1))
+			sieve[j - 1] = true;
 	}
 
 	// Remaining numbers != -1 are prime.	
 	for(unsigned int i = 0; i < N; i++)
-		if(sieve[i] != -1) cout << sieve[i] << endl;
+		if(!sieve[i]) cout << i + 1 << endl;
 
 	return 0;
 }
