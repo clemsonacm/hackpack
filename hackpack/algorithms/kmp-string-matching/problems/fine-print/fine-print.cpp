174620787ca712bbd0419954c45bc0075d55c763
Marshall Clyburn <mardev2013@gmail.com>
2014-10-26 01:05:03 -0400

    Add a lot of commenting to KMP code.

--- hackpack/algorithms/kmp-string-matching/problems/fine-print/fine-print.cpp
+++ hackpack/algorithms/kmp-string-matching/problems/fine-print/fine-print.cpp
@@ -23,14 +23,31 @@ int main(int numberOfArgs, char** args)
 		// Consider a possible match
 		if(text[i] == keyword[0])
 		{
+			// Number of characters that can be skipped due to
+			// character mismatches with the first character of
+			// the keyword.
 			unsigned int skip = 0;
+
+			// Whether or not a prospective match was present while
+			// matching another substring.
 			bool using_submatch = false;
+
+			// What the submatch is matching and also where the match
+			// should continue should a mismatch occur with the first
+			// substring.
 			unsigned int submatch_idx = 0;
+
+			// Where the prospective match substring begins.
 			unsigned int submatch_beg = 0;
+
 			for(unsigned int m = 1; m <= keyword.length() && i + m < text.length(); m++)
 			{
+				// 'i + m' is the character currently under consideration.
+
 				if(m == keyword.length()) { match_idx = i; break; } // Complete match.
 
+				// If a possible submatch exists starting at this index,
+				// begin a submatch process for it.
 				if(!using_submatch && text[i + m] == keyword[0])
 				{
 //					cout << "Possible submatch: " << text.substr(i + m, 4) << endl;
@@ -38,14 +55,13 @@ int main(int numberOfArgs, char** args)
 					submatch_idx = 1;
 					submatch_beg = i + m;
 				}
-				else
+				else // Submatch process in progress concurrently.
 				{
-					if(text[i + m] == keyword[submatch_idx]) submatch_idx++;
-					if(text[i + m] != keyword[submatch_idx]) using_submatch = false;
+					if(text[i + m] == keyword[submatch_idx]) submatch_idx++; // Submatched matched character.
+					if(text[i + m] != keyword[submatch_idx]) using_submatch = false; // Submatch mismatch.
 				}
 				
 				if(text[i + m] == keyword[m]) skip++; // Matched character.
-				else break; // Mismatch.
 				else // Mismatch.
 				{
 //					cout << "Mismatch occured." << endl;
