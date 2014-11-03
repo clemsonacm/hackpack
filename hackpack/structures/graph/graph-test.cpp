4f055758665750890473b601fd8c6fb8a7e5fb47
robertu94 <robertu@clemson.edu>
2014-11-02 19:46:06 -0500

    Improved api

--- hackpack/structures/graph/graph-test.cpp
+++ hackpack/structures/graph/graph-test.cpp
@@ -7,6 +7,7 @@ int main(int argc, char *argv[])
 	g.insert(1,2,3);
 	g.insert(2,3,4);
 	g.insert(1,3,4);
-
+	for(auto i = g.begin(1); i != g.end(1); i++){
+		cout << i->first << i->second << endl;}
 	return 0;
 }
--- hackpack/structures/graph/graph.cpp
+++ hackpack/structures/graph/graph.cpp
@@ -1,15 +1,24 @@
 #include<unordered_map>
 using namespace std;
+
+//The graph class uses c++11 extensions; use map to get O(log N) time with c++98
 class sparse_graph{
 	unordered_map < int, unordered_map< int, int> > g;
 	public:
-	void insert(int s, int d, int e){
-		if(g.find(s) == g.end()) g.emplace(s,unordered_map<int,int>());
-		if(g.find(d) == g.end()) g.emplace(d,unordered_map<int,int>());
-		g.at(s).emplace(d,e); }
-	bool empty(){return g.empty();}
-	size_t size(){return g.size();}
-	unordered_map<int, int>::iterator begin(int s){ return g.at(s).begin(); }
-	unordered_map<int, int>::iterator end(int s){ return g.at(s).end(); }
-	sparse_graph(): g() {};
+		//inserts into the graph in O(1) am ex time
+		void insert(int s, int d, int e){g[s][d] = e;g[d];}
+
+		//gets a specific edge in O(1) am ex time
+		unordered_map<int,int>::iterator find(int s, int d){
+			return g.find(s)->second.find(d); 
+		}
+
+		//returns an begin iterator for all edges leaving s; O(K) time to traverse
+		unordered_map<int, int>::iterator begin(int s){ return g.find(s)->second.begin(); }
+		
+		//returns an end iterator for all edges leaving s; O(K) time to traverse
+		unordered_map<int, int>::iterator end(int s){ return g.find(s)->second.end(); }
+
+		//Constructs the sparse graph
+		sparse_graph(): g() {};
 };
