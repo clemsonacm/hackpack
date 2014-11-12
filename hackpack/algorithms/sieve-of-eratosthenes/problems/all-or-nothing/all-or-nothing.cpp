e88e23e19e67aa70b42418b357e3d3bc1c3a0b19
Marshall Clyburn <marshall@mardev.net>
2014-11-11 23:16:02 -0500

    Include 0 in the sequence.
    This manages to clean up the code quite a bit by removing a +/-1
    here and there.

--- hackpack/algorithms/sieve-of-eratosthenes/problems/all-or-nothing/all-or-nothing.cpp
+++ hackpack/algorithms/sieve-of-eratosthenes/problems/all-or-nothing/all-or-nothing.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 using namespace std;
 
-const unsigned int N = 10000000;
+const unsigned int N = 10000001;
 bitset<N> sieve;
 unsigned int prefix[N];
 
@@ -14,14 +14,15 @@ int main()
 		
 	// Begin at index 1. Starting the sift at index 0 (1) will
 	// cause all numbers to be marked composite.
-	sieve[0] = 1; // 1 is not prime.
+	sieve[0] = 1; // 0 is not prime.
+	sieve[1] = 1; // 1 is not prime.
 	for(unsigned int i = 1; i < N; i++)
 	{
 		// If this number has been marked, do not use it.
 		if(sieve[i]) continue;
 		// Mark composites of the prime.
-		for(unsigned int j = pow(i + 1, 2); j <= N; j += (i + 1))
-			sieve[j - 1] = 1;
+		for(unsigned int j = pow(i, 2); j <= N; j += (i))
+			sieve[j] = 1;
 	}
 
 	// Compute prefix sum of the number of primes.
@@ -33,8 +34,8 @@ int main()
 	}
 
 	// Remaining numbers != 0 are prime.	
-	if(a < 2) cout << prefix[b - 1];
-	else cout << prefix[b - 1] - prefix[a - 2];
+	if(a < 2) cout << prefix[b];
+	else cout << prefix[b] - prefix[a - 1];
 	cout << endl;
 
 	return 0;
