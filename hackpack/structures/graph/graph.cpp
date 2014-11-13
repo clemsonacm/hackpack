35e7c34d54b830da81b4080f4d55cb011bd52a40
Robert Underwood <robertu@clemson.edu>
2014-11-13 13:05:33 -0500

    Initial list of applications

--- hackpack/structures/graph/graph.cpp
+++ hackpack/structures/graph/graph.cpp
@@ -1,6 +1,8 @@
 #include<unordered_map>
 using namespace std;
 
+typedef unordered_map<int,int> umii;
+typedef unordered_map< int , umii > umiumii;
 //The graph class uses c++11 extensions; use map to get O(log N) time with c++98
 class sparse_graph{
 	unordered_map < int, unordered_map< int, int> > graph;
@@ -9,22 +11,22 @@ class sparse_graph{
 		void insert(int source, int destination, int edge){graph[source][destination] = edge;graph[destination];}
 
 		//graphets a specific edgraphe in O(1) am ex time
-		unordered_map<int,int>::iterator find(int source, int destination){
+		umii::iterator find(int source, int destination){
 			return graph.find(source)->second.find(destination); 
 		}
 
 		//returns an begraphin iterator for all edges leaving source; O(K) time to traverse
-		unordered_map<int, int>::iterator begin(int source){ return graph.find(source)->second.begin(); }
+		umii::iterator begin(int source){ return graph.find(source)->second.begin(); }
 		
 		//returns an end iterator for all edges leaving source; O(K) time to traverse
-		unordered_map<int, int>::iterator end(int source){ return graph.find(source)->second.end(); }
+		umii::iterator end(int source){ return graph.find(source)->second.end(); }
 
         //returns begin iterator over all of the nodes
-        unordered_map<int, unordered_map<int,int> >::iterator begin () {return graph.begin();}
+        unordered_map<int, umii >::iterator begin () {return graph.begin();}
 
         //returns end iterator over all of the nodes
-        unordered_map<int, unordered_map<int,int> >::iterator end () {return graph.end();}
+        umiumii::iterator end () {return graph.end();}
         
-		//Constructs the sparse graphraph
+		//Constructs the sparse graph
 		sparse_graph(): graph() {};
 };
