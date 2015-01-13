2ab7c33cf17deb7d416f2c5d6f6cec61808b06d7
PeterofOrange <prschat@g.clemson.edu>
2015-01-13 14:53:32 -0500

    Readding arctan and triangle area

---
+++ hackpack/algorithms/computational-geometry/arctan.cpp
@@ -0,0 +1,24 @@
+<<<<<<< HEAD
+#include<iostream>
+#include<cmath>		/* atan */
+#define PI 3.14159265
+
+double arctan(double param) {
+	double result;
+	result = atan (param) * 180 / PI; //Remove the " * 180 / PI" to convert to degrees.
+	//fprintf(stdout, "The arc tangent of %.1f is %.1f degrees\n", param, result );
+	return result;
+}
+=======
+#include<iostream>
+#include <cmath>		/* atan */
+#define PI 3.14159265
+
+int main() {
+	double param, result;
+	param = 1.0;
+	result = atan (param) * 180 / PI; //Remove the " * 180 / PI" to convert to degrees.
+	fprintf(stdout, "The arc tangent of %.1f is %.1f degrees\n", param, result );
+	return 0;
+}
+>>>>>>> 54f62fb475ff714691c744b1d2f4934192f72762
---
+++ hackpack/algorithms/computational-geometry/tri-area.cpp
@@ -0,0 +1,34 @@
+<<<<<<< HEAD
+#include<iostream>
+#include<cmath>
+
+double tri_area(double *p1, double *p2, double *p3){
+	double v1[2], v2[2];
+	v1[0] = p2[0]-p1[0];
+	v2[0] = p3[0]-p1[0];
+	v1[1] = p2[1]-p1[1];
+	v2[1] = p3[1]-p1[1];
+
+	double det = (v1[0]*v2[1]) - (v1[1]*v2[0]);
+	return det/2;
+}
+
+
+=======
+#include<iostream>
+#include<cmath>
+
+int main(){
+	double p1[2]={0,0}, p2[2]={0,3}, p3[2]={4,0}; //Three 2d points to measure the area of the triangle between
+	double v1[2], v2[2];
+	v1[0] = p2[0]-p1[0];
+	v2[0] = p3[0]-p1[0];
+	v1[1] = p2[1]-p1[1];
+	v2[1] = p3[1]-p1[1];
+
+	int det = (v1[0]*v2[1]) - (v1[1]*v2[0]);
+	int area = det/2;
+}
+
+
+>>>>>>> 54f62fb475ff714691c744b1d2f4934192f72762
