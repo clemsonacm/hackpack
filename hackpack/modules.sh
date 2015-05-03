79e217d0381a1ae2e6aaf3d504ec37fd49c71e00
Robert Underwood <robertu@clemson.edu>
2015-05-02 22:23:15 -0400

    added test harness for unittesting

---
+++ hackpack/modules.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#A test harness that runs all the test cases for the modules
+
+toplevel=$(pwd)
+for module in $(find . -name Makefile  -not -path ./Makefile)
+do
+    cd $(dirname $module)
+    make $1 2>&1 |grep -v "make"|grep -v "g\+\+"
+    cd $toplevel
+done
